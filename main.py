a6717e1fcd9e624049d7eb70eb0ccd872caa5b20
Mehmet Akif KARADAĞ <88200933+Makifkaradag@users.noreply.github.com>
2024-09-07 21:17:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ Created on Sat Sep  7 20:51:44 2024
 """
 from ultralytics import YOLO
 
-# Eğitilmiş modelinizi yükleyin
+
 model = YOLO("weight.pt")  # Model dosyasının yolunu doğru şekilde belirtin
 
 # Tahmin yapılacak resmi yükleyin
 image_path = "tumor.jpg"
 
-# Nesne tespiti yap
+
 results = model.predict(image_path, save=True)
 
-# Her bir sonucu liste içinde döndürdüğü için döngü ile tespitleri gezelim
-for result in results:
-    # Her bir tespit edilen nesnenin detaylarını yazdıralım
-    print(result.boxes.xyxy)  # Bounding box koordinatları
-    print(result.boxes.conf)  # Konfidans skorları
-    print(result.boxes.cls)   # Sınıf etiketleri (örneğin tümör)
 
-# Sonuçlar kaydedildiği için yolunu da gösterebiliriz
+for result in results:
+    
+    print(result.boxes.xyxy)  
+    print(result.boxes.conf)  
+    print(result.boxes.cls)   
+
+
 print(f"Sonuçlar kaydedildi: {results[0].save_dir}")
